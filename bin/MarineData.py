a28e635915a51ac1873effe0e58c2eb423513a63
casablanca <evaseemefly@126.com>
2017-12-02 16:37:28 +0800

    使用抽象工厂的方式创建realtime和perclock对象 两个data中存在三个同名的方法

---
+++ bin/MarineData.py
@@ -0,0 +1,17 @@
+'''
+思路：
+分钟和整点数据均有:
+-1 获取文件全路径集合的方法，统一为getTargetFullNameList
+-2 读取文件中内容的方式，统一命名为readData
+-3 验证数据内容的方法，统一命名为validate
+'''
+
+
+class ReadTimeData:
+    def __init__(self):
+        pass
+
+class PerclockData:
+    def __init__(self):
+        pass
+
