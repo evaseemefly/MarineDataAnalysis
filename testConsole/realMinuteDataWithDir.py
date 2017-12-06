da4039e7321b35d33f50b3ab135f085520a6cb55
casablanca <evaseemefly@126.com>
2017-12-06 16:12:07 +0800

    加入部分备注及列表推导

--- testConsole/realMinuteDataWithDir.py
+++ testConsole/realMinuteDataWithDir.py
@@ -5,10 +5,18 @@ datafile_path = './data/xxx/realtime/2017/3/06/'
 
 #获取指定路径中所有文件相对路径
 def GetFolderFile(folderPath):
+    '''
+    可以直接在方法冒号后打三个点，回车，pycharm会自动补齐其余的说明
+    获取指定路径中所有文件相对路径
+    :param folderPath:
+    :return:
+    '''
     filePathes=[]
     for root,dirs,files in os.walk(folderPath):
-        for f in files:
-            filePathes.append(folderPath+f)
+        # 此处可以用列表推导
+        [filePathes.append(folderPath+f) for f in files]
+        # for f in files:
+        #     filePathes.append(folderPath+f)
 
     return filePathes
 
@@ -22,6 +30,9 @@ def GetFileData(filePathList):
         with open(singleFile) as txt:
             lst = txt.readlines()
             result = ''.join(lst)
+            # 此处没看明白
+            # 你是读取每一行的数据匹配是否包含英文字母，若包含则将英文字母替换为空值
+            # 将多个空格替换为一个空格
             result =  regex_s.sub(' ',regex_c.sub('',result))
             resultDataList.append(result)
 
