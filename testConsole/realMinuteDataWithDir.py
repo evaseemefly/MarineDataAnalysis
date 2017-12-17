b54f880913e3554e113ae65ab639146200e8a077
stupidanimal <stupidanimal@outlook.com>
2017-12-18 02:34:59 +0800

    Update realMinuteDataWithDir.py

--- testConsole/realMinuteDataWithDir.py
+++ testConsole/realMinuteDataWithDir.py
@@ -1,49 +1,54 @@
+import numpy as np
+import pandas as pd
 import os
 import re
+from io import StringIO
+datafile_path = '../data/xxx/realtime/2017/11/01/'
 
-datafile_path = './data/xxx/realtime/2017/3/06/'
+table_index='DT,WT,SL,WL,DT1,AT,BP,HU,RN,WS1,WS2,WS3,WS4,WS5,WS6,WS7,WS8,WS9,WS10'.split(',')
 
 #获取指定路径中所有文件相对路径
 def GetFolderFile(folderPath):
     '''
-    可以直接在方法冒号后打三个点，回车，pycharm会自动补齐其余的说明
     获取指定路径中所有文件相对路径
     :param folderPath:
     :return:
     '''
-    filePathes=[]
-    for root,dirs,files in os.walk(folderPath):
-        # 此处可以用列表推导
-        [filePathes.append(folderPath+f) for f in files]
-        # for f in files:
-        #     filePathes.append(folderPath+f)
+    file_pathes=[]
+    for root, dirs, files in os.walk(datafile_path, topdown=False):
+        if(len(files)!=0):
+            for name in files:
+                file_pathes.append(root+'/'+name)
 
-    return filePathes
+    return file_pathes
 
 
 #将文件夹内路径读成一条内容并合并成list返回
 def GetFileData(filePathList):
-    resultDataList=[]
-    regex_c = re.compile('[A-Z]*')
-    regex_s = re.compile('[\s]+')
+    '''
+    读取txt文件并转换为dataframe，暂时先转换成lst，还没想好怎么合并，指定好列了应该好合并
+    '''
+    lst=[]
+    reg_removeHead = re.compile('[A-Z]*?[\s]+')
+    reg_removeEnter = re.compile('\n')
     for singleFile in filePathList:
-        with open(singleFile) as txt:
-            lst = txt.readlines()
-            result = ''.join(lst)
-            # 此处没看明白
-            # 你是读取每一行的数据匹配是否包含英文字母，若包含则将英文字母替换为空值
-            # 将多个空格替换为一个空格
-            result =  regex_s.sub(' ',regex_c.sub('',result))
-            resultDataList.append(result)
-
-    return resultDataList
-
-
-filelist = GetFolderFile(datafile_path)
-data = GetFileData(filelist)
-
-for line in data:
-    print(line)
+        try:
+            with open(singleFile) as txt:
+                result = txt.read()
+                result = reg_removeEnter.sub('', result) #移除换行
+                result = reg_removeHead.sub(' ', result) #移除标题等待重新指定
+                result = result.lstrip(' ')
+                tempdf=pd.read_csv(StringIO(result),delimiter='\s',header=None,names=table_index)
+                tempdf.columns=table_index
+                lst.append(tempdf)
+        except Exception as err:
+            print(singleFile)
+            print(err)
+            
+    return lst
 
 
-    
+#'这下边是测试'
+temp = ['../data/xxx/realtime/2017/11/01/18/SQ201711011832.11754','../data/xxx/realtime/2017/11/01/00/SQ201711010001.11754']
+
+result = GetFileData(temp)
