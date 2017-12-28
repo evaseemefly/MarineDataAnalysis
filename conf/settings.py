25f5e25c95900a2a9a5b131bf48b7a842d542024
liusihan-单位 <evaseemefly@126.com>
2017-12-28 15:36:32 +0800

    1

--- conf/settings.py
+++ conf/settings.py
@@ -6,8 +6,8 @@ BASE_DIR=os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # 海洋站的原始路径
 # mac 上的地址
 # SOURCE_PATH=r"/Users/liusihan/Documents/GitHub/learn_sourcecode_DataAnalysis/codes_bymyself/data/"
-SOURCE_PATH=r"/Users/liusihan/Documents/重要测试数据/sanya"
-# SOURCE_PATH=r"E:\03协同开发\单位海洋站数据分析\data"
+# SOURCE_PATH=r"/Users/liusihan/Documents/重要测试数据/sanya"
+SOURCE_PATH=r"E:\03协同开发\单位海洋站数据分析\data"
 
 # 输出的路径
 TARGET_PATH=r"E:\03协同开发\单位海洋站数据分析\result\201711.csv"
